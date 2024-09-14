11ea6805643f5af1b665a4f5fe6f4aea56ec4b77
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-15 00:39:03 +0100

    addinterest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# 3 variables principal and rate and time
+# calculate the formel I=P*R*T
+# print the simple interest is: [interest]
+principal= 1000
+rate= 0.05
+time= 3
+interest= str(principal*rate*time) 
+print ("the simple interest is: "+(interest))
