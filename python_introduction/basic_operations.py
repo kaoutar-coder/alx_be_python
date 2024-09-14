b6d963b19d7e8d17d6c9b96a6dc516d01cfaf0f3
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-14 23:44:51 +0100

    correct

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,9 @@
 # creat a 2 variables number1 and number2 
 # performe the operations addition subtraction and multiplication on these numbers
 # result [operation] of [number1] abd [number2] is [result]
+print ("assuming number1 = 10 and number2 = 5")
 number1= 10
-"number1= 10"
 number2= 5
-"number2= 5"
 result= str(number1+number2)
 print ("addition"+" of "+str(number1)+" and "+str(number2)+" is "+(result))
 result1= str(number1-number2)
