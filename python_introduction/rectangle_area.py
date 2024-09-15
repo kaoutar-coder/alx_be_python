7352ce820b4ff962a53972ac0bbdac2ded4f6db7
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-15 19:51:53 +0100

    calculatearea

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,11 @@
+# project is calculate the area of rectangle 
+# 1 create a new file
+# 2 create 2 variables length and width
+# 3 calculate the formula Area= lengh * width
+# 4 variable area
+# 5 pint  the area oh the rectangle is a : [area]
+
+length=10
+width=5
+area=str(length*width) 
+print ("the area oh the rectangle is a :"+(area))
