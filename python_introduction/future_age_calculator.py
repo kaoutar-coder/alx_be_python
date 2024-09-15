66d1e1eff07b27dad64f78d80e6c0a6bb44da0b4
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-15 23:57:01 +0100

    futureage

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# The script prompts the user for their current age.
+# The user enters their age.
+# The script calculates and prints how old the user will be in 2050.
+
+user_age = (input("How old are you ?"))
+age = int(user_age) + 27
+print ("in 2050, you will be " + str (age) + " yers old.")
