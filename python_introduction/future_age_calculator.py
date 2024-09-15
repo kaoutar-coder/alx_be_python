3c14f3106098ab80882065a824976b4181efae98
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-16 00:12:16 +0100

    correctagecalculate

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -2,6 +2,6 @@
 # The user enters their age.
 # The script calculates and prints how old the user will be in 2050.
 
-age = input("How old are you ?")
+age = input("How old are you? ")
 age2 = int(age) + 27
-print ("In 2050, you will be " + str(age2) + " years old.")
+print("In 2050, you will be " + str(age2) + " years old.")
