4c6b5b7e1a91969a98001957e2193a9916639b1e
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-25 17:41:07 +0100

    reminderdily

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,26 +1,22 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
-Task = input("enter your task: ")
-Priority = input("priority (high/medium/low): ")
-time_bound = input("it is time-bound ? (yes/no): ")   
-
-match Priority:
+match priority:
     case "high":
-        reminder=f"Finish"'{Task}'" is a high priority task"
+        reminder = f"'{task}' is a {priority} task"
     case "medium":
-        reminder=f"reminder:Finish"'{task}'" is a medium priority task"
+        reminder = f"'{task}' is a {priority} task"
     case "low":
-        reminder=f"Note:" '{Task}'"is a low priority task."
+        reminder = f"'{task}' is a {priority} task"
     case _:
-        reminder = f"⚠️ '{Task}' has no priority level"
+        reminder = f"⚠️ '{task}' has no priority level"
 
-# Add the time-bound condition
 if time_bound == "yes":
     reminder += " that requires immediate attention today!"
-    print(reminder)
-elif time_bound == "no":
-    reminder += " Consider completing it when you have free time."
-    print(reminder)
+    print("Reminder: " + reminder)
+else:
+    reminder += ". Consider completing it when you have free time."
+    print("Note: " + reminder)
 
-
-    
 
