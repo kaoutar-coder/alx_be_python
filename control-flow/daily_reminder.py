64a7a7aa71df60832f31b394b87cf420b1210e2f
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-25 17:34:10 +0100

    reminderdl

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,15 +1,17 @@
-Task=input("enter your task: ")
-Priority=input("priority high/medium/low: ")
-Time_Bound=input("it is time-bound ? (yes/no): ")
+
+Task = input("enter your task: ")
+Priority = input("priority (high/medium/low): ")
+Time_Bound = input("it is time-bound ? (yes/no): ")
+
 match Priority:
     case "high":
-        reminder=f"Finish"'{task}'" is a high priority task"
+        reminder=f"Finish"'{Task}'" is a high priority task"
     case "medium":
         reminder=f"reminder:Finish"'{task}'" is a medium priority task"
     case "low":
-        reminder=f"Note:" '{task}'"is a low priority task."
-
-        exit()  # Stop the script if an invalid priority is entered
+        reminder=f"Note:" '{Task}'"is a low priority task."
+    case _:
+        reminder = f"⚠️ '{Task}' has no priority level"
 
 # Add the time-bound condition
 if Time_Bound == "yes":
