7fe384a55a926b62ddc06cece4c838c84f54196c
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-09-25 17:25:03 +0100

    reminderd

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,21 @@
-task=input("enter your task: ")
-priority=input("priority high/medium/low: ")
-time_bound=input("it is time-bound ? (yes/no): ")
-match priority:
+Task=input("enter your task: ")
+Priority=input("priority high/medium/low: ")
+Time_Bound=input("it is time-bound ? (yes/no): ")
+match Priority:
     case "high":
-        reminder=f"Finish"+task+" is a high priority task"
+        reminder=f"Finish"'{task}'" is a high priority task"
     case "medium":
-        reminder=f"reminder:Finish"+task+" is a medium priority task"
+        reminder=f"reminder:Finish"'{task}'" is a medium priority task"
     case "low":
-        reminder=f"Note:"+ task + "is a low priority task."
+        reminder=f"Note:" '{task}'"is a low priority task."
 
         exit()  # Stop the script if an invalid priority is entered
 
 # Add the time-bound condition
-if time_bound == "yes":
+if Time_Bound == "yes":
     reminder += " that requires immediate attention today!"
     print(reminder)
-elif time_bound == "no":
+elif Time_Bound == "no":
     reminder += " Consider completing it when you have free time."
     print(reminder)
 
