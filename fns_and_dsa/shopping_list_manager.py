248b03a796adab62ed945baf262dc507a09fc1ec
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-10-03 00:58:18 +0100

    choppinglists

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,11 +14,11 @@ def main():
         choice = input("Enter your choice: ")
         
         if choice == '1':
-            item= input("enter the item name to add:  ")
+            item= input("Enter the item to add:  ")
             shopping_list.append(item)
 
         elif choice == '2':
-            item= input("enter the item name to remove:  ")
+            item= input("Enter the item to remove:  ")
             shopping_list.remove()
             
         elif choice == '3':
@@ -27,7 +27,7 @@ def main():
         elif choice == '4':
             print("Goodbye!")
             break
-        
+
         else:
             print("Invalid choice. Please try again.")
 
