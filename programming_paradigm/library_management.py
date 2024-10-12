794faae3c8ee291ee8ba3ec8c06716ed2840c54f
kaoutar bayassef <kaoutarbayassef.93@gmail.com>
2024-10-12 05:34:47 +0100

    book

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,35 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out_book(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+class Library:
+
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.check_out_book()
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.return_book()
+
+    def list_available_books(self):
+        for book in self._books:
+            if book._is_checked_out == False:
+                print(f"{book.title} by {book.author}")
